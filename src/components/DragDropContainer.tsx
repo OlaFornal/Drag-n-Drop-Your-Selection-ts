7033fa0b880256147a984f2af458a1f5914b35ef
Ola <aleksandra@pland.co>
2025-10-22 11:06:45 +0200

    Components created and dnd and tailwind installed

---
+++ src/components/DragDropContainer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DragDropContainer = () => {
+  return (
+    <div>DragDropContainer</div>
+  )
+}
+
+export default DragDropContainer
---
+++ src/components/DroppableArea.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DroppableArea = () => {
+  return (
+    <div>DroppableArea</div>
+  )
+}
+
+export default DroppableArea
